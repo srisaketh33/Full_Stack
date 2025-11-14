b935cbc65b4151b9fc7657f139c2b9835a10891e
SRI TULASI SATYA SAKETH.R <117180580+srisaketh33@users.noreply.github.com>
2025-11-14 22:44:38 +0530

    Delete app/__ini__.py

--- app/__ini__.py
+++
@@ -1,3 +0,0 @@
-MONGODB_URI=mongodb+srv://srisaketh33_db_user:18Saketh10@cluster0.xn4ppya.mongodb.net/?appName=Cluster0
-DB_NAME=mydb
-SECRET_KEY=pP8mZ4hT3v9Q2nL8xR5cD0kW1sE7uV4bH2jN9mA6qF3tG8yC1lD7pK0rS5fV2bX
